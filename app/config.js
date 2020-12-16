47a072e181876f03e7b995def247e7701bcaac33
ben raine <ben.raine.digitaldwp@gmail.com>
2020-12-16 13:49:33 +0000

    updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Kickstart',
+  serviceName: 'Kickstart applications tracker',
 
   // Default port that prototype runs on
   port: '3000',
